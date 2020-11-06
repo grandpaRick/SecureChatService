5a427e958f834de1dee7e9100a04d3cf13585304
grandpaRick <desmondumebeh@gmail.com>
2020-11-06 21:07:39 +0000

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,8 @@
+import java.math.BigInteger;
+
+public class Application {
+    public static void main(String[] args) {
+        BigInteger bigInteger = new BigInteger("5");
+        System.out.println(Long.MAX_VALUE);
+    }
+}
---
+++ src/TTP.java
@@ -0,0 +1,2 @@
+public class TTP {
+}
